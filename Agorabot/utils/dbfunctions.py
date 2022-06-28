1f7d4cb669a4fa5f689f9c2d7eeff9f9d79d3c3c
MR AGORA <106959345+MR-AGORA@users.noreply.github.com>
2022-06-28 12:48:54 +0530

    Update dbfunctions.py

--- Agorabot/utils/dbfunctions.py
+++ Agorabot/utils/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from Tianabot.mongo import db
+from Agorabot.mongo import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
