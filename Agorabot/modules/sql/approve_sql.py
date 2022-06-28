2212cc6e3b847eea65ed04530707dac003919009
MR AGORA <106959345+MR-AGORA@users.noreply.github.com>
2022-06-28 13:05:37 +0530

    agora server update

--- Agorabot/modules/sql/approve_sql.py
+++ Agorabot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, BigInteger, func, distinct
 
-from Tianabot.modules.sql import BASE, SESSION
+from Agorabot.modules.sql import BASE, SESSION
 
 class Approvals(BASE):
     __tablename__ = "approval"
